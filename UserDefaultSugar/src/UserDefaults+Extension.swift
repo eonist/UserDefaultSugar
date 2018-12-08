686abd2e1bf6b0e0e4dba7ea7403a644988b568c
Eonist <30n1st@gmail.com>
2018-12-08 21:53:49 +0100

    Init

--- UserDefaultSugar/src/UserDefaults+Extension.swift
+++ UserDefaultSugar/src/UserDefaults+Extension.swift
@@ -1,20 +1,22 @@
 import Foundation
 
-extension UserDefaults {
-    /**
-     * Reads all (key,val) pairs in UserDefaults.standard
-     */
-    static func debug(){
-        UserDefaults.standard.dictionaryRepresentation().forEach { (key, value) in
-            print("Key: \(key) value: \(value)")
-        }
-    }
-    /**
-     * Removes all keys and values from user defaults
-     */
-    static func removeAll() {
-        UserDefaults.standard.dictionaryRepresentation().forEach{  (key, _) in
-            UserDefaults.standard.removeObject(forKey: key)
-        }
-    }
+public extension UserDefaults {
+   /**
+    * Reads all (key,val) pairs in UserDefaults.standard
+    */
+   public static func debug(){
+      UserDefaults.standard.dictionaryRepresentation().forEach { (key, value) in
+         print("Key: \(key) value: \(value)")
+      }
+   }
+   /**
+    * Removes all keys and values from user defaults
+    * NOTE: Seems to not remove the apple data. like keyboar etc. Only what the user set
+    */
+   public static func removeAll() {
+      UserDefaults.standard.dictionaryRepresentation().forEach{  (key, _) in
+         UserDefaults.standard.removeObject(forKey: key)
+      }
+   }
 }
+
---
+++ UserDefaultsExample/ViewController.swift
@@ -0,0 +1,50 @@
+import UIKit
+import UserDefaultSugar
+
+class ViewController: UIViewController {
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      //      testSettingValues()
+//      testReadingValues()
+      removeDataTest()
+   }
+}
+extension ViewController {
+   /**
+    * Test setting
+    */
+   func testSettingValues(){
+      let defaults = UserDefaults.standard
+      defaults.set(25, forKey: "Age")
+      defaults.set(true, forKey: "hasOnboarded")
+      defaults.set(CGFloat.pi, forKey: "Pi")
+      defaults.set("Paul Hudson", forKey: "Name")
+      defaults.set(Date(), forKey: "LastRun")
+      //arr and dict:
+      let array = ["Hello", "World"]
+      defaults.set(array, forKey: "SavedArray")
+      
+      let dict = ["Name": "Paul", "Country": "UK"]
+      defaults.set(dict, forKey: "SavedDict")
+//
+   }
+   /**
+    *
+    */
+   func testReadingValues(){
+      UserDefaults.standard.set("John", forKey: "FirstName")
+      Swift.print(UserDefaults.standard.object(forKey: "FirstName"))//John
+      //where is the userdefaults stored?
+      
+   }
+   /**
+    *
+    */
+   func removeDataTest(){
+      testSettingValues()
+//      UserDefaults.debug()
+      Swift.print("remove all")
+      UserDefaults.removeAll()
+      UserDefaults.debug()
+   }
+}
