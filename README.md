7ac259e0e9adbcbda23e1f3cbccbbe85322ed328
Eonist <30n1st@gmail.com>
2018-12-08 11:56:38 +0100

    Init

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # UserDefaultSugar
+
+### What is it:
 UserDefault sugar
+
+### How do I use it:
+`UserDefaults.debug()`//Prints all data  
+`UserDefaults.removeAll()`//removes all data
+
+### How can I get it:
+- Carthage: `github "eonist/UserDefaultSugar" "master"`
+
+### Blog post about using userDefaults
+[http://eon.codes/blog/2018/09/18/userdefaults/](http://eon.codes/blog/2018/09/18/userdefaults/)
+
+### Resources
+[https://github.com/radex/SwiftyUserDefaults](https://github.com/radex/SwiftyUserDefaults)
---
+++ UserDefaultSugar/UserDefaultSugar.h
@@ -0,0 +1,19 @@
+//
+//  UserDefaultSugar.h
+//  UserDefaultSugar
+//
+//  Created by Eon on 08/12/2018.
+//  Copyright © 2018 FutureLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UserDefaultSugar.
+FOUNDATION_EXPORT double UserDefaultSugarVersionNumber;
+
+//! Project version string for UserDefaultSugar.
+FOUNDATION_EXPORT const unsigned char UserDefaultSugarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UserDefaultSugar/PublicHeader.h>
+
+
