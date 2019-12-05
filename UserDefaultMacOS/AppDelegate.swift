b9be7d9e6ca4203112cd5fdaf9609a9140aec41a
Eonist <30n1st@gmail.com>
2019-12-06 00:41:38 +0100

    Lint pt.3

--- UserDefaultMacOS/AppDelegate.swift
+++ UserDefaultMacOS/AppDelegate.swift
@@ -30,7 +30,7 @@ open class View: NSView {
    /**
     * Boilerplate
     */
-   required public init?(coder decoder: NSCoder) {
+   public required init?(coder decoder: NSCoder) {
       fatalError("init(coder:) has not been implemented")
    }
 }
