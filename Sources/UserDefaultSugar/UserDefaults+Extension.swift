aa6b9f673e6e871bbcf2aed02924e2a5b38c1b2a
eonist <30n1st@gmail.com>
2020-12-26 14:26:17 +0100

    Add codable support

--- Sources/UserDefaultSugar/UserDefaults+Extension.swift
+++ Sources/UserDefaultSugar/UserDefaults+Extension.swift
@@ -19,3 +19,30 @@ extension UserDefaults {
       }
    }
 }
+/**
+ * UserDefaults extensions
+ */
+extension UserDefaults {
+   /**
+    * Set Codable object into UserDefaults
+    * - Parameters:
+    *   - object: Codable Object
+    *   - forKey: Key string
+    * - Throws: UserDefaults Error
+    */
+   public func set<T: Codable>(object: T, forKey: String) throws {
+      let jsonData = try JSONEncoder().encode(object)
+      set(jsonData, forKey: forKey)
+   }
+   /**
+    * Get Codable object into UserDefaults
+    * - Parameters:
+    *   - object: Codable Object
+    *   - forKey: Key string
+    * - Throws: UserDefaults Error
+    */
+   public func get<T: Codable>(objectType: T.Type, forKey: String) throws -> T? {
+      guard let result = value(forKey: forKey) as? Data else { return nil }
+      return try JSONDecoder().decode(objectType, from: result)
+   }
+}
