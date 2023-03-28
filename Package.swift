416403e841b11fabfd9daf3810a3bc827cd609aa
eonist <30n1st@gmail.com>
2023-03-28 03:39:38 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,11 +1,9 @@
-// swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.6
 import PackageDescription
 
 let package = Package(
     name: "UserDefaultSugar",
-    platforms: [.iOS(.v12), .macOS(.v10_13)],
+    platforms: [.iOS(.v15), .macOS(.v12)],
     products: [
         .library(
             name: "UserDefaultSugar",
